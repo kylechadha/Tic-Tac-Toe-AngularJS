e03504bf05f530ee3cc1cee42d9c7d9661adc8b9
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-23 22:26:00 -0800

    Add reset functionality

--- js/main.js
+++ js/main.js
@@ -1,25 +1,32 @@
 function TicTacCtrl ($scope) {
   $scope.cells = [['','',''],['','',''],['','','']];
-  var turn = {val: true}, counter = 0, header = document.getElementsByTagName('h1'), xWin = "XXX", oWin = "OOO";
+  var turn = {val: true}, counter = 0, header = document.getElementsByTagName('h1'), win = false, xWin = "XXX", oWin = "OOO";
 
   $scope.playerMove = function(r, c) {
-    if($scope.cells[r][c] == "") {
+    if($scope.cells[r][c] == "" && win == false) {
       if (turn.val) {
-        event.target.className = 'xmove';
+        // event.target.className = 'xmove';
         $scope.cells[r][c] = 'X';
-        counter++;
       } else {
-        event.target.className = 'omove';
+        // event.target.className = 'omove';
         $scope.cells[r][c] = 'O';
-        counter++;
       }
+      console.log(counter);
+      counter++;
       turn.val = !turn.val;
       $scope.winAnalysis();
     }
   }
 
   $scope.winAnalysis = function() {
-    var row1 = $scope.cells[0].join(""), row2 = $scope.cells[1].join(""), row3 = $scope.cells[2].join(""), col1 = $scope.cells[0][0] + $scope.cells[1][0] + $scope.cells[2][0], col2 = $scope.cells[0][1] + $scope.cells[1][1] + $scope.cells[2][1], col3 = $scope.cells[0][2] + $scope.cells[1][2] + $scope.cells[2][2], diag1 = $scope.cells[0][0] + $scope.cells[1][1] + $scope.cells[2][2], diag2 = $scope.cells[0][2] + $scope.cells[1][1] + $scope.cells[2][0];
+    var row1 = $scope.cells[0].join("");
+    var row2 = $scope.cells[1].join("");
+    var row3 = $scope.cells[2].join("");
+    var col1 = $scope.cells[0][0] + $scope.cells[1][0] + $scope.cells[2][0];
+    var col2 = $scope.cells[0][1] + $scope.cells[1][1] + $scope.cells[2][1];
+    var col3 = $scope.cells[0][2] + $scope.cells[1][2] + $scope.cells[2][2];
+    var diag1 = $scope.cells[0][0] + $scope.cells[1][1] + $scope.cells[2][2];
+    var diag2 = $scope.cells[0][2] + $scope.cells[1][1] + $scope.cells[2][0];
 
     if (xWin == row1 || xWin == row2 || xWin == row3 || xWin == col1 || xWin == col2 || xWin == col3 || xWin == diag1 || xWin == diag2) {
       header[0].innerHTML = "Congrats, X Wins!";
@@ -30,7 +37,13 @@ function TicTacCtrl ($scope) {
     } else if (counter == 9) {
       header[0].innerHTML = "Draw x_x";
     }
+  }
 
+  $scope.gameReset = function() {
+    $scope.cells = [['','',''],['','',''],['','','']];
+    header[0].innerHTML = "Tic Tac Toe";
+    counter = 0;
+    win = false;
   }
 
 }
