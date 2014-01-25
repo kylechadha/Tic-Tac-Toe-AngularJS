d86e964ac0b8adb593e5c2d9010e108a7e3c71df
Kyle Chadha <kyle.chadha@gmail.com>
2014-01-25 14:40:21 -0800

    Limit player to own moves only

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+// To Do List 
+// 2. Add player 1 and player 2 notation & scoreboard
+// 3. Fix reset button
+// 4. Add disappearing functionality
+// 5. Clean up CSS & refactor
+
 var app = angular.module("TicTac", ["firebase"])
 app.controller("TicTacCtrl", function($scope, $firebase) {
   
@@ -5,15 +11,19 @@ app.controller("TicTacCtrl", function($scope, $firebase) {
   $scope.fbRoot = $firebase(gameRef);
 
   var IDs;
+  var player;
   var xWin = "XXX";
   var oWin = "OOO";
   var header = document.getElementsByTagName('h1');
+  $scope.animation = true;
 
   $scope.fbRoot.$on("loaded", function() {
     IDs = $scope.fbRoot.$getIndex();
+
     if (IDs.length == 0) {
       $scope.fbRoot.$add( {
         cells: [['','',''],['','',''],['','','']],
+        loadCount: 0,
         turn: true,
         counter: 0,
         win: false
@@ -22,23 +32,39 @@ app.controller("TicTacCtrl", function($scope, $firebase) {
       $scope.fbRoot.$on("change", function() {
         IDs = $scope.fbRoot.$getIndex();
         $scope.game = $scope.fbRoot.$child(IDs[0]);
+        player = "X";
       });
     } else {
-      //Why is there no on change function here (for player 2)? Or is this not for player 2?
+      //Why is there no on change function here (for player 2)?
       $scope.game = $scope.fbRoot.$child(IDs[0]);
-    }
-  });
+      player = "O";
+    };
 
+    //Why is $scope.game still undefined at this point?!
+    // $scope.game.loadCount++;
+    // if ($scope.game.loadCount == 1) {
+    //   player = 'X';
+    // } else if ($scope.game.loadCount == 2) {
+    //   player = 'O';
+    // } else {
+    //   player = 'Spectator';
+    // }
+  });
+  
   $scope.playerMove = function(r, c) {
-    if($scope.game.cells[r][c] == "" && $scope.game.win == false) {
-      if ($scope.game.turn) {
+    if ($scope.game.cells[r][c] == "" && $scope.game.win == false) {
+      if ($scope.game.turn && player == 'X') {
         $scope.game.cells[r][c] = 'X';
-      } else {
-        $scope.game.cells[r][c] = 'O';
+        $scope.game.counter++;
+        $scope.game.turn = !$scope.game.turn;
+      } else if (!$scope.game.turn && player == 'O') {
+        $scope.game.cells[r][c] = 'O';      
+        $scope.game.counter++;
+        $scope.game.turn = !$scope.game.turn;
       }
-      $scope.game.counter++;
-      $scope.game.turn = !$scope.game.turn;
       $scope.winAnalysis();
+      $scope.animation = false;
+      $scope.game.$save();
       // setTimeout($scope.revertCell(r, c), 5000);
     }
   }
@@ -54,7 +80,6 @@ app.controller("TicTacCtrl", function($scope, $firebase) {
     var diag2 = $scope.game.cells[0][2] + $scope.game.cells[1][1] + $scope.game.cells[2][0];
 
     if (xWin == row1 || xWin == row2 || xWin == row3 || xWin == col1 || xWin == col2 || xWin == col3 || xWin == diag1 || xWin == diag2) {
-      header[0].innerHTML = "Congrats, X Wins!";
       $scope.game.win = true;
     } else if (oWin == row1 || oWin == row2 || oWin == row3 || oWin == col1 || oWin == col2 || oWin == col3 || oWin == diag1 || oWin == diag2) {
       header[0].innerHTML = "Congrats, O Wins!";
@@ -64,16 +89,24 @@ app.controller("TicTacCtrl", function($scope, $firebase) {
     }
   }
 
+  // $scope.$watch('$scope.game.win', function(newValue, oldValue) {
+  //   if ($scope.game.win == true && (player == 'X' || player == 'Spectator')) {
+  //     header[0].innerHTML = "Congrats, X Wins!";
+  //   } else {
+  //     header[0].innerHTML = "Better luck next time!"
+  //   }
+  // })
+
   // $scope.revertCell = function() {
     // $scope.cells[r][c] = '';
   // }
 
-  $scope.gameReset = function() {
-    $scope.game.cells = [['','',''],['','',''],['','','']];
-    header[0].innerHTML = "Tic Tac Toe";
-    $scope.game.turn = true;
-    $scope.game.win = false;
-    $scope.game.counter = 0;
-  }
+  // $scope.gameReset = function() {
+  //   $scope.game.cells = [['','',''],['','',''],['','','']];
+  //   header[0].innerHTML = "Tic Tac Toe";
+  //   $scope.game.turn = true;
+  //   $scope.game.win = false;
+  //   $scope.game.counter = 0;
+  // }
 
 });
